4e675dba024f0526faa65179da340757b9090034
Samiha-hash <mubaswirasamiha@gmail.com>
2025-02-12 12:07:05 +0600

    problem fixed

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOpts = {
-    origin: ['https://cerulean-kitten-16952f.netlify.app', 'http://localhost:5173'],
+    origin: ['https://car-rental-12345.netlify.app', 'http://localhost:5173'],
     methods: [
         "GET",
         "POST",
